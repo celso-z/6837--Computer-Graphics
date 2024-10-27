1a2d2661f7657a7d7ba845aa5a5092ca4f3f3699
Celso Z. <celso.zacarias@estudante.ufjf.br>
2024-10-27 16:49:34 -0300

    Implemented custom matrix stack entity

--- two/MatrixStack.cpp
+++ two/MatrixStack.cpp
@@ -3,17 +3,26 @@
 MatrixStack::MatrixStack()
 {
 	// Initialize the matrix stack with the identity matrix.
+	this->m_matrices.push_back(Matrix4f(1.0f, 0.0f, 0.0f, 0.0f,
+										  0.0f, 1.0f, 0.0f, 0.0f,
+										  0.0f, 0.0f, 1.0f, 0.0f,
+										  0.0f, 0.0f, 0.0f, 1.0f));
 }
 
 void MatrixStack::clear()
 {
 	// Revert to just containing the identity matrix.
+	this->m_matrices.clear();	
+	this->m_matrices.push_back(Matrix4f(1.0f, 0.0f, 0.0f, 0.0f,
+										  0.0f, 1.0f, 0.0f, 0.0f,
+										  0.0f, 0.0f, 1.0f, 0.0f,
+										  0.0f, 0.0f, 0.0f, 1.0f));
 }
 
 Matrix4f MatrixStack::top()
 {
 	// Return the top of the stack
-	return Matrix4f();
+	return this->m_matrices.back();
 }
 
 void MatrixStack::push( const Matrix4f& m )
@@ -21,9 +30,13 @@ void MatrixStack::push( const Matrix4f& m )
 	// Push m onto the stack.
 	// Your stack should have OpenGL semantics:
 	// the new top should be the old top multiplied by m
+	Matrix4f resulting_matrix = this->m_matrices.back() * m;
+	this->m_matrices.push_back(resulting_matrix);
+
 }
 
 void MatrixStack::pop()
 {
 	// Remove the top element from the stack
+	this->m_matrices.pop_back();
 }
